961437b18ab7af03786958085e3b8c3d3bbe8df0
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2020-10-17 16:38:40 +0200

    Add changelog in AsmLib.asm

--- AsmLib/AsmLib.asm
+++ AsmLib/AsmLib.asm
@@ -1,11 +1,24 @@
-;komentarz
+;			Filtr Gaussa v0.1.2
+;		
+;		autor: Kamil D³ugajczk
+;
+;	Changelog:
+;	- 12.10.2020 - wersja 0.1.0 - utoworzenie projektu wraz z repozytorium na Githubie
+;	- 15.10.2020 - wersja 0.1.1 - utworzenie szkicu projektu: prostego GUI, stworzenie 
+;				   dwóch dodatkowych projektów - biblioteki asemblerowej oraz C++ - 
+;				   wraz z funkcjami "zaœlepkami". Skonfigurowanie oraz przetestowanie
+;				   dynamicznego linkowania bibliotek.
+;	- 17.10.2020 - wesja 0.1.2 - poprawa GUI: dodanie miniaturek wybranej bitmapy, 
+;				   pola wyboru pliku decelowego, pola do zwrócenia czasu wykonania
+;				   oraz pola do wyœwietlenia histogramów
+;
+;
+;
 
 
-
-
-.code
-initAsm proc
-	mov rax, 2
-	ret
-initAsm endp
-end
+.code						; blok kodu
+initAsm proc				; procedura initAsm bêd¹ca funkcj¹ zaœlepk¹
+	mov rax, 2				; przenieœ wartoœæ 2 do rejestru rex
+	ret						; powrót z procedury
+initAsm endp				; koniec procedury
+end							; koniec bloku kodu
